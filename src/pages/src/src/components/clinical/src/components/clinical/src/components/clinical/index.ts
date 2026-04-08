8a476bb3d00d294df542a71035b433d626ff21f0
Revanth-kr4890 <kr4890@srmist.edu.in>
2026-04-08 12:26:52 +0530

    Add index.ts for clinical component exports

---
+++ src/pages/src/src/components/clinical/src/components/clinical/src/components/clinical/index.ts
@@ -0,0 +1,6 @@
+export { VitalCard, vitalCardVariants } from "./VitalCard";
+export { StatusIndicator, statusIndicatorVariants } from "./StatusIndicator";
+export { AlertBanner, alertBannerVariants } from "./AlertBanner";
+export { ClinicalMetric, clinicalMetricVariants } from "./ClinicalMetric";
+export { PatientHeader } from "./PatientHeader";
+export { SectionPanel } from "./SectionPanel";
